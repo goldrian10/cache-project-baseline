f2a69002492c63e05d25ecd2fea53b4700c22ef5
Loriana <lorisanabria@gmail.com>
2020-05-27 00:12:23 +0000

    Update L1Obl.h

--- include/L1Obl.h
+++ include/L1Obl.h
@@ -22,7 +22,10 @@
  * [in] loadstore: type of operation false if true if store
  * [in] debug: if set to one debug information is printed
  *
- * [in/out] cache_block: return the cache operation return (miss_hit_status)
+ * [in/out] cache_blocks: l1 cache line to work on 
+ * [in/out] cache_blocks_obl: l1 cache line where obl will be located
+ * [in/out] cache_blocks_result: return the cache operation result in L1 (miss_hit_status)
+ * [in/out] cache_blocks_result_obl: return the cache operation result in L1 for obl block (miss_hit_status)
  * [out] result: result of the operation (returns_types)
  */
 int lru_obl_replacement_policy (int idx,
@@ -30,8 +33,8 @@ int lru_obl_replacement_policy (int idx,
                                 int associativity,
                                 bool loadstore,
                                 entry* cache_block,
-				entry* cache_block_obl,
+				                entry* cache_block_obl,
                                 operation_result* operation_result_cache_block,
-				operation_result* operation_result_cache_obl,
+				                operation_result* operation_result_cache_obl,
                                 bool debug=false);
 #endif
